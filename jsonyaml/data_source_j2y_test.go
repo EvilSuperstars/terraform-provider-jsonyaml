d0c5495bc6251a3af4587d4a0193585608169f50
Kit Ewbank <Kit.Ewbank@capitalone.com>
2017-11-20 15:58:59 -0500

    Acceptance test improvements.

--- jsonyaml/data_source_j2y_test.go
+++ jsonyaml/data_source_j2y_test.go
@@ -1,9 +1,12 @@
 package jsonyaml
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	"gopkg.in/yaml.v2"
 )
 
 const testDataSourceJ2YConfig_basic = `
@@ -27,15 +30,63 @@ EOS
 `
 
 func TestDataSourceJ2Y_basic(t *testing.T) {
+	var v interface{}
+
 	resource.UnitTest(t, resource.TestCase{
 		Providers: testProviders,
 		Steps: []resource.TestStep{
 			{
 				Config: testDataSourceJ2YConfig_basic,
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttrSet("data.jsonyaml_j2y.foo", "yaml"),
+					testAccCheckYamlExists("data.jsonyaml_j2y.foo", "yaml", &v),
+					testAccCheckYamlStructure(&v),
 				),
 			},
 		},
 	})
+}
+
+func testAccCheckYamlExists(name, key string, v *interface{}) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[name]
+		if !ok {
+			return fmt.Errorf("Not found: %s", name)
+		}
+
+		is := rs.Primary
+		if is == nil {
+			return fmt.Errorf("No primary instance: %s", name)
+		}
+		if is.ID == "" {
+			return fmt.Errorf("No ID is set: %s", name)
+		}
+
+		y, ok := is.Attributes[key]
+		if !ok {
+			return fmt.Errorf("%s: Attribute '%s' not found", name, key)
+		}
+
+		return yaml.Unmarshal([]byte(y), v)
+	}
+}
+
+func testAccCheckYamlStructure(y *interface{}) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		switch v := (*y).(type) {
+		case map[interface{}]interface{}:
+			k1 := v["Key1"]
+			if k1.(string) != "Value1" {
+				return fmt.Errorf("Incorrect value for Key1: %q", k1)
+			}
+			k2 := v["Key2"]
+			if len(k2.([]interface{})) != 2 {
+				return fmt.Errorf("Incorrect length for Key2's value: %q", k2)
+			}
+			break
+		default:
+			return fmt.Errorf("Incorrect top-level type: %T", v)
+		}
+
+		return nil
+	}
 }
--- jsonyaml/data_source_y2j_test.go
+++ jsonyaml/data_source_y2j_test.go
@@ -1,9 +1,12 @@
 package jsonyaml
 
 import (
+	"encoding/json"
+	"fmt"
 	"testing"
 
 	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
 )
 
 const testDataSourceY2JConfig_basic = `
@@ -24,15 +27,63 @@ EOS
 `
 
 func TestDataSourceY2J_basic(t *testing.T) {
+	var v interface{}
+
 	resource.UnitTest(t, resource.TestCase{
 		Providers: testProviders,
 		Steps: []resource.TestStep{
 			{
 				Config: testDataSourceY2JConfig_basic,
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttrSet("data.jsonyaml_y2j.foo", "json"),
+					testAccCheckJsonExists("data.jsonyaml_y2j.foo", "json", &v),
+					testAccCheckJsonStructure(&v),
 				),
 			},
 		},
 	})
+}
+
+func testAccCheckJsonExists(name, key string, v *interface{}) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[name]
+		if !ok {
+			return fmt.Errorf("Not found: %s", name)
+		}
+
+		is := rs.Primary
+		if is == nil {
+			return fmt.Errorf("No primary instance: %s", name)
+		}
+		if is.ID == "" {
+			return fmt.Errorf("No ID is set: %s", name)
+		}
+
+		j, ok := is.Attributes[key]
+		if !ok {
+			return fmt.Errorf("%s: Attribute '%s' not found", name, key)
+		}
+
+		return json.Unmarshal([]byte(j), v)
+	}
+}
+
+func testAccCheckJsonStructure(j *interface{}) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		switch v := (*j).(type) {
+		case map[string]interface{}:
+			k1 := v["Key1"]
+			if k1.(string) != "Value1" {
+				return fmt.Errorf("Incorrect value for Key1: %q", k1)
+			}
+			k2 := v["Key2"]
+			if len(k2.([]interface{})) != 2 {
+				return fmt.Errorf("Incorrect length for Key2's value: %q", k2)
+			}
+			break
+		default:
+			return fmt.Errorf("Incorrect top-level type: %T", v)
+		}
+
+		return nil
+	}
 }
